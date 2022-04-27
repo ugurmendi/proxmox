932aec120bac7b7f04a61beb73dd9fa08636ea06
ugurm <ugur.mendi@comodo.com>
2022-04-27 16:30:06 +0300

    proxmox terraform creating vm's

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "template_name" {
+    default = "k9s-clone"
+}
+variable "ssh_key" {
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDSKAUM9ScAdFmQLZONsfqOx4TPFHkq4J8kGWIkd2DNPk3wRL7h1tRxjd6TPQWQSaHmMBm3niOpqrbEs2rCMJFdWxb2TxJePxD/hlLmmDkw70/axZNn7GLnecnvsKooyUqPGnkod3QF+ubZO49Tes6MZB4e3t4n0qMj+jrQISfp6YDJhDlkxeKInFPrNYwaLGwVIDoV31fLB8rbfpTYdf73l4gpXvzxsTxpnWQTeJXKpyR93Cogq75nvQ3sRFtzaUSq6RN4DmRjsMWM//YL/LEMTQbuGlsWAB6ARUtG8ZIiWJ8SbE8/1EsfITBN5ACgIIRPwlxLVvDpb+Hzoud++fSGmE2WeAYPkHcUMqsJT/jodj3G95ZW0t7Sd8OCIpIQr7xMooMVtMRW9LFe9ypFopjdhHYsxnKr1jFxobrEMkSTLYGpiBWiRwXPJKXUz1PS3EQteHRZvFJ7s7LXgwo4qm1KDgttFifMdFZtHt+4Q6Q+n2oc3sG5ZrmqFLt/Fb6rQs= ugurm@ugurm-HP-ProBook-450-G5"
+}
+variable "proxmox_host" {
+    default = "trdevopsprox"
+}
